379067214238180fde046b5dbc4b16df4f4f9500
Tony Kay <tony.g.kay@gmail.com>
2023-10-26 15:52:19 -0600

    Restructured like showroom_template_zero

--- utilities/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-
-echo "Starting build process..."
-podman run --platform linux/amd64 -v "./:/antora" antora/antora site.yml
-echo "Build process complete. Check the ./www folder for the generated site."
